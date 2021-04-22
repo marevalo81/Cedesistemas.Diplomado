49d05564a3dc42c2a5764021de94909aeccbe975
Melina Arevalo <Melina.Arevalo@viamericas.com>
2021-04-21 19:16:04 -0500

    versionde hoy

---
+++ Diplomado.Net/apiRest/Extensions/ServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiRest
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Extension for configure cors on api
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+    }
+}
